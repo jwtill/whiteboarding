64a99094419b41f2b4ed23517ab2cda9c86cca2b
Josh Tillinghast <jwtill@icloud.com>
2022-04-15 09:31:58 -0700

    Refactor notebook #1 into problem1.js

--- problem1.js
+++ problem1.js
@@ -1,2 +1,14 @@
 //! Question #1: Turning Strings to URLs
 //URLs cannot have spaces. Instead, all spaces in a string are replaced with %20. Write an algorithm that replaces all spaces in a string with %20.
+
+function stringUrl(stringSpace) {
+  let stringArr = stringSpace.split("");
+  const addTwenty = stringArr.map((char) => {
+    if (char === " ") {
+      return "%20";
+    } else {
+      return char;
+    }
+  });
+  return addTwenty.join("");
+}
