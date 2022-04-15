b93dca52fdec433bae6ef828a90bd7c4531efb95
Josh Tillinghast <jwtill@icloud.com>
2022-04-15 09:41:45 -0700

    Take a break from recursion #1

--- problem1.js
+++ problem1.js
@@ -1,6 +1,7 @@
 //! Question #1: Turning Strings to URLs
 //URLs cannot have spaces. Instead, all spaces in a string are replaced with %20. Write an algorithm that replaces all spaces in a string with %20.
 
+//!With .map
 function stringUrl(stringSpace) {
   let stringArr = stringSpace.split("");
   const addTwenty = stringArr.map((char) => {
@@ -12,3 +13,13 @@ function stringUrl(stringSpace) {
   });
   return addTwenty.join("");
 }
+
+//!With Recursion work on this?????
+function stringUrl(stringArr, i) {
+  if (char[i] === " ") {
+    stringUrl("%20", i++);
+  } else {
+    stringUrl(stringArr[i], i++);
+  }
+}
+//
