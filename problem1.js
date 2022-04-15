da69729ffe951a421192d9dd57fee4dd2ec64901
Josh Tillinghast <jwtill@icloud.com>
2022-04-15 09:03:31 -0700

    Add initial question 1 notebook image

---
+++ problem1.js
@@ -0,0 +1,2 @@
+//! Question #1: Turning Strings to URLs
+//URLs cannot have spaces. Instead, all spaces in a string are replaced with %20. Write an algorithm that replaces all spaces in a string with %20.
